56f8da4706ac36ec188211b79b8cacb972afd5b1
José Renan <joserenansl99@gmail.com>
2017-03-11 17:45:35 -0300

    Adicionado javadoc aos métodos de ParticipacaoController e TipoParticipacao

--- src/br/edu/ufcg/projetolp2/controllers/ParticipacaoController.java
+++ src/br/edu/ufcg/projetolp2/controllers/ParticipacaoController.java
@@ -19,21 +19,54 @@ public class ParticipacaoController {
 		participacoes = new HashSet<>();
 	}
 
-	public void associaProfessor(Pessoa pessoa, Projeto projeto, boolean coordenador, double valorHora, int qntHoras) {
-		Participacao participacao = new Participacao(projeto, pessoa, qntHoras, valorHora, new ParticipacaoProfessor(coordenador));
+	/**
+	 * Método responsável por associar um professor à um projeto.
+	 * 
+	 * @param professor - objeto pessoa a ser associado
+	 * @param projeto - objeto projeto ao qual o professor fará parte
+	 * @param coordenador - boolean informando se o professor será coordenador do projeto
+	 * @param valorHora - double representando o valor da hora de trabalho do professor
+	 * @param qntHoras - int representando a quantidade de horas semanais que o professor trabalhará
+	 */
+	public void associaProfessor(Pessoa professor, Projeto projeto, boolean coordenador, double valorHora, int qntHoras) {
+		Participacao participacao = new Participacao(projeto, professor, qntHoras, valorHora, new ParticipacaoProfessor(coordenador));
 		participacoes.add(participacao);
 	}
 
-	public void associaGraduando(Pessoa pessoa, Projeto projeto, double valorHora, int qntHoras) {
-		Participacao participacao = new Participacao(projeto, pessoa, qntHoras, valorHora, new ParticipacaoGraduando());
+	/**
+	 * Método responsável por associar um graduando à um projeto.
+	 * 
+	 * @param graduando - objeto pessoa a ser associado
+	 * @param projeto - objeto projeto ao qual o graduando fará parte
+	 * @param valorHora - double representando o valor da hora de trabalho do graduando
+	 * @param qntHoras - int representando a quantidade de horas semanais que o graduando trabalhará
+	 */
+	public void associaGraduando(Pessoa graduando, Projeto projeto, double valorHora, int qntHoras) {
+		Participacao participacao = new Participacao(projeto, graduando, qntHoras, valorHora, new ParticipacaoGraduando());
 		participacoes.add(participacao);
 	}
 
-	public void associaProfissional(Pessoa pessoa, Projeto projeto, String cargo, double valorHora, int qntHoras) {
-		Participacao participacao = new Participacao(projeto, pessoa, qntHoras, valorHora, new ParticipacaoProfissional());
+	/**
+	 * Método responsável por associar um profissional à um projeto.
+	 * 
+	 * @param graduando - objeto pessoa a ser associado
+	 * @param projeto - objeto projeto ao qual o graduando fará parte
+	 * @param valorHora - double representando o valor da hora de trabalho do graduando
+	 * @param qntHoras - int representando a quantidade de horas semanais que o graduando trabalhará
+	 * @param cargo - String representando o cargo do profissional no projeto
+	 */
+	public void associaProfissional(Pessoa profissional, Projeto projeto, String cargo, double valorHora, int qntHoras) {
+		Participacao participacao = new Participacao(projeto, profissional, qntHoras, valorHora, new ParticipacaoProfissional());
 		participacoes.add(participacao);
 	}
 
+	/**
+	 * Método responsável por remover a associação que uma pessoa tem em um determinado projeto
+	 * 
+	 * @param pessoa - pessoa cuja participação deve ser removida
+	 * @param projeto - projeto cuja participação de determinada pessoa deve ser removida
+	 * @throws AssociacaoException caso a pessoa não esteja associada ao determinado projeto
+	 */
 	public void removeParticipacao(Pessoa pessoa, Projeto projeto) throws AssociacaoException {
 		
 		for (Iterator<Participacao> iterator = participacoes.iterator(); iterator.hasNext();) {
--- src/br/edu/ufcg/projetolp2/model/participacao/tipos/TipoParticipacao.java
+++ src/br/edu/ufcg/projetolp2/model/participacao/tipos/TipoParticipacao.java
@@ -7,5 +7,15 @@ public interface TipoParticipacao {
 	public static final String GRADUANDO = "GRADUANDO";
 	public static final String PROFESSOR_COORDENADOR = "PROFESSOR COORDENADOR";
 	
+	/**
+	 * Método que retorna uma string informando qual o tipo de participação da pessoa no projeto
+	 * @return String que pode ser:
+	 * <ul>
+	 * 	<li>{@value #PROFESSOR}</li>
+	 *  <li>{@value #PROFISSIONAL}</li>
+	 *  <li>{@value #GRADUANDO}</li>
+	 *  <li>{@value #PROFESSOR_COORDENADOR}</li>
+	 * </ul>
+	 */
 	String getTipoParticipacao();
 }

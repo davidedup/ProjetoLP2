5e8072403526e97ab1343a2f97bb83695ef88a42
Juan Barros <juanlyrabarros@gmail.com>
2017-03-19 22:06:02 -0300

    CooperacaoTests prontos

--- test/br/edu/ufcg/projetolp2/projeto/CooperacaoTests.java
+++ test/br/edu/ufcg/projetolp2/projeto/CooperacaoTests.java
@@ -5,25 +5,32 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.edu.ufcg.projetolp2.model.pessoa.Pessoa;
+import br.edu.ufcg.projetolp2.model.projeto.PedFactory;
+import br.edu.ufcg.projetolp2.model.projeto.Projeto;
+import br.edu.ufcg.projetolp2.model.projeto.tipos.Cooperacao;
+import br.edu.ufcg.projetolp2.model.projeto.tipos.Pet;
+
 public class CooperacaoTests {
+	Cooperacao projCoop;
 
 	@Before
 	public void setUp() throws Exception {
+		PedFactory factory  = new PedFactory();
+		projCoop = (Cooperacao) factory.create(112, "coop", "coop", 3, 3, 3, "sSHSAO", "12/12/2011", 1);
 	}
-
-	@Test
-	public void testCooperacao() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	public void testGetInfo() {
-		fail("Not yet implemented");
+		assertEquals("", projCoop.getInfo("coordenador"));
+		projCoop.setCoordenador(new Pessoa("num", "uhi@gmial.com", "921.123.312-32"));
+		assertEquals("num", projCoop.getInfo("coordenador"));
 	}
 
 	@Test
 	public void testSetInfo() {
-		fail("Not yet implemented");
+		projCoop.setCoordenador(new Pessoa("num", "uhi@gmial.com", "921.123.312-32"));
+		assertEquals("num", projCoop.getInfo("coordenador"));
 	}
 
 }
